436bd8fe8bd7a41e1ac52b98f95033186a612076
= <olukayodefadairo@gmail.com>
2023-05-26 10:45:22 +0100

    first commit

---
+++ contracts/WrappedBitcoin.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract WrappedBitcoin is ERC20 {
+    constructor() ERC20('WrappedBitcoin', 'WBTC'){
+        _mint(msg.sender, 5000 * 10**18);
+    }
+
+}
---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.9",
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  }
+};
+ 
